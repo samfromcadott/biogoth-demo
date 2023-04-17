ae3cb94d28271018989b5858e4e1ec067f6e55a3
samfromcadott <samahstan@gmail.com>
2023-04-17 10:36:26 -0500

    Moving player rectangle

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,40 @@
 #include <raylib/raylib-cpp.hpp>
 
-int main() {
-	int screenWidth = 800;
-	int screenHeight = 450;
+using namespace raylib;
 
-	raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
-	// raylib::Texture logo("raylib_logo.png");
+int main() {
+	int screen_width = 1280;
+	int screen_height = 720;
+
+	Window window(screen_width, screen_height, "raylib-cpp - basic window");
 
 	SetTargetFPS(60);
 
+	raylib::Rectangle player_rect(100, 100, 64, 128);
+	raylib::Rectangle floor_rect(200, 200, 128, 64);
+
+	raylib::Vector2 player_velocity(0.0, 0.0);
+
 	while ( !window.ShouldClose() ) {
+		player_velocity = raylib::Vector2(0.0, 0.0);
+		const float speed = 5.0;
+
+		if ( IsKeyDown(KEY_RIGHT))  player_velocity.x += speed;
+		if ( IsKeyDown(KEY_LEFT) ) player_velocity.x -= speed;
+		if ( IsKeyDown(KEY_UP) ) player_velocity.y -= speed;
+		if ( IsKeyDown(KEY_DOWN) ) player_velocity.y += speed;
+
+		player_rect.SetPosition( player_rect.GetPosition() + player_velocity );
+
 		BeginDrawing();
 
-			window.ClearBackground(RAYWHITE);
+			window.ClearBackground(raylib::RAYWHITE);
 
-			DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+			player_rect.Draw(raylib::VIOLET);
+			floor_rect.Draw(raylib::GRAY);
 
 		EndDrawing();
 	}
-
-	// UnloadTexture() and CloseWindow() are called automatically.
 
 	return 0;
 }
