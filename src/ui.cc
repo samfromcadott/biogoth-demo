18957cd1334b992bed76297b72ee3c761c61d489
samfromcadott <samahstan@gmail.com>
2023-04-21 14:28:31 -0500

    Change health text to heath bar

--- src/ui.cc
+++ src/ui.cc
@@ -9,6 +9,11 @@
 void health_bar() {
 	auto view = registry.view<const Player, const Health>();
 	for ( auto [entity, player, health] : view.each() ) {
-		raylib::DrawText(""+std::to_string(health.now)+"/"+std::to_string(health.max), 32, 650, 32, raylib::BLACK);
+		raylib::Vector2 bar_origin(32, 550);
+		float max_height = 150;
+		float bar_width = 32;
+		float height_loss = max_height - ( (float(health.now) / float(health.max)) * max_height );
+
+		DrawRectangle(int(bar_origin.x), int(bar_origin.y+height_loss), bar_width, max_height-height_loss, RED);
 	}
 }
