4b10bbbf934d73284a2beacf1a20025f3a20e71f
samfromcadott <samahstan@gmail.com>
2023-04-27 06:15:50 -0500

    Organized systems.hh

--- src/systems.hh
+++ src/systems.hh
@@ -2,21 +2,32 @@
 
 #include "components.hh"
 
+// Player actions
 void player_move(); // Gets movement input for player
 void player_jump();
-void move_collide(); // Moves a body and applies collisions
-void gravity();
-void death();
-void render_colliders();
-void collider_overlap(); // Pushes colliders apart if they overlap
 void player_attack(); // Checks if player is attacking
 void player_bite(); // Checks if player is biting
+
+// Physics
+void move_collide(); // Moves a body and applies collisions
+void gravity();
+void collider_overlap(); // Pushes colliders apart if they overlap
+
+// Combat
 void melee_attack(); // Handles melee attacks
 void bite_attack();
 void bullets(); // Updates bullets
-void render_bullets();
+
+// Enemies
 void enemy_think();
+
+// General
+void death();
 void camera_update();
+
+// Render
+void render_colliders();
+void render_bullets();
 
 // UI Elements
 void health_bar();
