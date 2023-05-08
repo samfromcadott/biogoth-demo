20b7ba725bc87cd26c422c213a432e831d149521
samfromcadott <samahstan@gmail.com>
2023-05-08 12:50:29 -0500

    Correct fire rate for enemies

--- src/level.cc
+++ src/level.cc
@@ -21,7 +21,7 @@ void make_enemy(float x, float y, int direction) {
 	registry.emplace<Health>(enemy, 100, 100);
 	registry.emplace<Facing>(enemy, direction);
 	registry.emplace<Enemy>(enemy, 550.0f, 300.0f, 3.0f, true);
-	registry.emplace<GunAttack>(enemy, 3, 5, 0.5f, 15.0f, 1.0f, 0.0f);
+	registry.emplace<GunAttack>(enemy, 3, 5, 0.5f, 15.0f, 0.8333f, 0.0f);
 }
 
 void make_player(float x, float y, int direction) {
